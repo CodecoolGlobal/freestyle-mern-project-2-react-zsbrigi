b84a35a59357e8ea443cd53e582c56350841f5ba
Andor Margitics <144914187+mrgitics@users.noreply.github.com>
2024-02-07 14:30:16 +0100

    refactored components

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,9 @@ import './App.css';
 import reportWebVitals from './reportWebVitals';
 import MaingPage from './mainPage Components/MainPage.js';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import ChickenDishes from "./components/ChickenDishes.js";
-import BeefDishes from "./components/BeefDishes.js";
-import DessertDishes from "./components/DessertDishes.js";
-import PastaDishes from "./components/PastaDishes.js";
-import VegetarianDishes from "./components/VegetarianDishes.js";
+
+import BeefDishes from "./components/Dishes.js";
+
 
 
 const router = createBrowserRouter([
@@ -19,24 +17,8 @@ const router = createBrowserRouter([
     element: <MaingPage></MaingPage>
   },
   {
-    path: "/chicken",
-    element: <ChickenDishes></ChickenDishes>
-  },
-  {
-    path: "/beef",
-    element: <BeefDishes></BeefDishes>
-  },
-  {
-    path: "/pasta",
-    element: <PastaDishes></PastaDishes>
-  },
-  {
-    path: "/vegetarian",
-    element: <VegetarianDishes></VegetarianDishes>
-  },
-  {
-    path: "/dessert",
-    element: <DessertDishes></DessertDishes>
+    path: "/dishes/:dishType",
+    element: <BeefDishes/>
   }
 ])
 
