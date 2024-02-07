20b375553ddc1882785d16566b5f98f3a160c423
Andor Margitics <144914187+mrgitics@users.noreply.github.com>
2024-02-07 10:01:25 +0100

    beefDishmodelandComponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import BeefDishes from "./components/BeefDishes.js"
 
 function App() {
   return (
     <div className="App">
       <h1>The Cliens is working</h1>
+      <BeefDishes/>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import BeefDish from "./model/BeefDishes.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const PORT = 5000;
+
+
 
 async function connectToMongoose() {
     await mongoose.connect(process.env.URL);
@@ -16,6 +19,17 @@ connectToMongoose();
 app.get("/", (req, res) => {
     res.send('The server is running.');
 });
+
+app.get("/api/beefDishes", async (req,res) => {
+    
+        const beefDishes = await BeefDish.find()
+        res.json(beefDishes)
+   
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`This server is running on PORT ${PORT}`);
